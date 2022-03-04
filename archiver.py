cd7e22e3dc0f2c412561ec8b9a2873eb890cdfde
gviezcas <gviezcas@miners.utep.edu>
2022-03-04 11:31:12 -0700

    Added method readByteArray to read byte array into a file.

--- archiver.py
+++ archiver.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import os, sys
-
+#Method to turn file into byte array.#
 def writeByteArray(fd):
     try:
         with open(fd, "rb") as f:
@@ -12,14 +12,38 @@ def writeByteArray(fd):
         print("Error while opening file...")
         sys.exit(1)
 
-#def readByteArray(fd):
-    
+        
+#Method to turn byte array back into file.#
+def readByteArray(fd, byteArray):
+    try:
+        with open(fd, "wb") as f:
+            f.write(byteArray)
+    except IOError:
+        print("Error while opening file...")
+        sys.exit(1)
 
+#Below is just to test the methods above using the README file.#
+print("Attempting to archive file...\n")
+try:
+    os.remove("test.md")
+except:
+    pass
 path = "README.md"
+size = os.path.getsize(path)
 if os.path.exists(path):
     byteArray = writeByteArray(path)
     print(byteArray)
+    print("---------------------------------------------------------------")
+    print("Attempting to unarchive file...\n")
+    readByteArray("test.md", byteArray)
+    try:
+        with open("test.md", 'r') as f:
+            print(f.read())
+    except FileNotFoundError:
+        print("File could not be found or open...")
+        sys.exit(1)
 else:
     print("File %s does not exist." % path)
     sys.exit(1)
 print("\nDone...")
+sys.exit(0)
